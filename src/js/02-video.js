10d2fec904595b3b1e89198869883a5faeccc389
lilyzeta <115415031+lilyzeta@users.noreply.github.com>
2023-06-24 13:07:40 -0400

    push

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const local = 'videoplayer-current-time';
+var throttled = _.throttle(renewToken, 1000, { trailing: false });
+jQuery(player).on('timeupdate', throttled);
+player.on('play', function () {
+  console.log('played the video!');
+});
 
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onPlay = function (data) {
+  localStorage.setItem(local, data.seconds.toString());
+};
+
+player.on('timeupdate', onPlay);
+
+function setCurrentTime() {
+  const savedTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+}
+player.setCurrentTime(savedTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,21 @@
+const input = document.getElementsByTagName('input');
+const ki = 'feedback-form-state';
+const getting = localStorage.getItem('ki');
+const submitt = document.getElementsByTagName('button');
 
+console.log(getting);
+
+if (getting != null) {
+  let input = getting;
+} else {
+  input = null;
+}
+
+function submission() {
+  localStorage.setItem(ki, input);
+  input.reset();
+}
+
+submitt.addEventListener('click', submission);
+
+console.log(input);
